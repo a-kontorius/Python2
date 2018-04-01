8aca3282add15c1c322134081e826e2a1ea8b74a
Adminus Kontorius <a-kontorius@yandex.ru>
2018-04-02 00:39:54 +0300

    Рабочий вариант сервера. Добавлена фукнция конвертации по параметрам. Нужно привести в порядок код

--- project/server.py
+++ project/server.py
@@ -1,7 +1,21 @@
 import sys
-import time
+import json
 from socket import *
 from argparse import ArgumentParser
+
+#ф-я преобразования цельсия в фаренгейт по заданным параметрам
+def cel_to_far_convert(options):
+    result = {}
+    min = options['min']
+    max = options['max']
+    step = options['step']
+
+    for с in range(min,max,step):
+        f = (с * 1.8) + 32
+        result[с] = float('{:.1f}'.format(f))
+
+    return result
+
 
 parser = ArgumentParser()
 parser.add_argument('-a', '--address', required=True, dest='address', help='server.py -a <address> [-p <port>]')
@@ -15,13 +29,20 @@ server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((address, port))
 server_socket.listen(5)
 
-print("Сервер запущен на адресе '{0}' и порту {1}".format(address,port))
-
 while True:
     client, address = server_socket.accept()
     print("Получен запрос от клиента %s" % str(address))
-    timestr = time.ctime(time.time()) + "\n"
-    client.send(timestr.encode('ascii'))
+
+    recive_buf = client.recv(1024)          # принимаем 1024 байта
+    json_buf = recive_buf.decode("utf-8")   # декодируем полученную информацию
+    client_options = json.loads(json_buf)   # десереализация
+
+    result = cel_to_far_convert(client_options)
+
+    server_answer = json.dumps(result)      # сереализация
+    buf = server_answer.encode()            # кодировка
+    client.send(buf)                        # оптаврялем ответ
+
     client.close()
 
 
