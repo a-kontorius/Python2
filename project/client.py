15cba24fd47d7313745313d9c2e9cb504a79bf32
Adminus Kontorius <a-kontorius@yandex.ru>
2018-04-02 00:40:26 +0300

    Рабочий вариант клиента. Нужно привести в порядок код

--- project/client.py
+++ project/client.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from socket import *
 
 if len(sys.argv) < 2:
@@ -6,17 +7,26 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 address = sys.argv[1]
-port = int(sys.argv[2]) if len(sys.argv) == 3 else 3333
+port = int(sys.argv[2]) if len(sys.argv) == 3 else 7777
+
+param = {
+    "min": 10,
+    "max": 53,
+    "step": 3
+}
+json_param = json.dumps(param)
+buf = json_param.encode("utf-8")
+
 
 client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((address, port))
+client_socket.send(buf)
 
-try:
-    client_socket.connect((address, port))
-    tm = client_socket.recv(1024)
-    print("Текущее время: %s" % tm.decode('ascii'))
-except ConnectionRefusedError as err:
-    print(err.strerror)
-
+answer_buf = client_socket.recv(1024)
+json_buf = answer_buf.decode("utf-8")
+answer = json.loads(json_buf)
+print(answer)
 client_socket.close()
+
 
 
